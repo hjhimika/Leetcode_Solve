cc62d92dfb7f8cd403d8b9bd7389d7feacf71296
Humayra Jahan <37900722+hjhimika@users.noreply.github.com>
2023-09-12 02:01:17 +0600

    Update 20. Valid Parentheses.cpp

--- 20. Valid Parentheses.cpp
+++ 20. Valid Parentheses.cpp
@@ -6,11 +6,13 @@ public:
         stack<char> stk;
         for(int i=0; i< s.size(); i++){
             if(s[i]=='(' || s[i]== '{' || s[i]== '[') stk.push(s[i]);
-            if( !stk.empty() && (stk.top()== '(' && s[i]== ')') || (stk.top()== '{' && s[i]== '}') || (stk.top()== '[' && s[i]== ']')){
-                    stk.pop();
-                }
-                
-            
+            else {
+                if(stk.empty()) return false;
+                else if( (stk.top()== '(' && s[i]== ')') 
+                || (stk.top()== '{' && s[i]== '}') 
+                || (stk.top()== '[' && s[i]== ']')) stk.pop();
+                else return false;
+            }
             
         }
      
